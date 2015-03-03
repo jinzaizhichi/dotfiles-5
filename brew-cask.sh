35d663e0b15af0b715a81822a34ab977176af6ef
Mario Andrés Yepes C <marioy47@gmail.com>
2015-03-03 12:38:31 -0500

    Adding brew-cask.sh from Paul Irish dotfiles and deleting some brew packages I don't need

---
+++ brew-cask.sh
@@ -0,0 +1,52 @@
+# Install native apps
+
+export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+sudo -v
+
+
+brew install caskroom/cask/brew-cask
+brew tap caskroom/versions
+
+# daily
+#brew cask install alfred
+#brew cask install divvy
+brew cask install dropbox
+#brew cask install gyazo
+#brew cask install onepassword
+#brew cask install rescuetime
+# cask install flux
+
+# dev
+brew cask install iterm2
+brew cask install sublime-text3
+brew cask install imagealpha
+brew cask install imageoptim
+
+# fun
+#brew cask install limechat
+#brew cask install miro-video-converter
+brew cask install simple-comic
+
+# browsers
+brew cask install google-chrome-canary
+#brew cask install firefox-nightly --force
+#brew cask install webkit-nightly --force
+#brew cask install chromium --force
+#brew cask install torbrowser
+
+# less often
+#brew cask install disk-inventory-x
+brew cask install screenflow4 # 4 specifically not 5.
+brew cask install vlc
+brew cask install keycastr  
+
+brew cask install flash
+
+# Development
+brew cask install sourcetree
+brew cask install github
+brew cask install phpstorm
+brew cask install virtualbox
+brew cask install vagrant
+brew cask install vagrant-manager
+
