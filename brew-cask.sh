9a2f90b4d9528fe90ff3ffa90fcb467e91c0ac69
Mario A Yepes <marioy47@gmail.com>
2015-03-09 20:01:13 -0500

    Adding alias and exports. Adding new casks

--- brew-cask.sh
+++ brew-cask.sh
@@ -22,9 +22,18 @@ brew cask install iterm2
 brew cask install sublime-text3
 brew cask install imagealpha
 brew cask install imageoptim
+brew cask install gimp
+brew cask install sourcetree
+brew cask install github
+brew cask install phpstorm
+brew cask install virtualbox
+brew cask install vagrant
+brew cask install vagrant-manager
+brew cask install boot2docker
+brew cask install rstudio
 
 # fun
-#brew cask install limechat
+brew cask install limechat
 #brew cask install miro-video-converter
 brew cask install simple-comic
 
@@ -34,6 +43,7 @@ brew cask install google-chrome
 #brew cask install webkit-nightly --force
 #brew cask install chromium --force
 #brew cask install torbrowser
+brew cask install flash
 
 # less often
 #brew cask install disk-inventory-x
@@ -41,14 +51,6 @@ brew cask install screenflow4 # 4 specifically not 5.
 brew cask install vlc
 brew cask install keycastr
 brew cask install android-file-transfer
+brew cask install mou
 
-brew cask install flash
 
-# Development
-brew cask install sourcetree
-brew cask install github
-brew cask install phpstorm
-brew cask install virtualbox
-brew cask install vagrant
-brew cask install vagrant-manager
-brew cask install boot2docker
