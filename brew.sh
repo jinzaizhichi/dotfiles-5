642878e854aa52d60d3a5d4ff5369590a0d37875
Mario A Yepes <marioy47@gmail.com>
2015-05-20 11:52:19 -0500

    Install vim with lua, python and ruby support

--- brew.sh
+++ brew.sh
@@ -42,7 +42,7 @@ brew install wget --with-iri
 #brew install narwhal
 
 # Install more recent versions of some OS X tools.
-brew install vim --override-system-vi
+brew install vim --override-system-vi --with-lua --with-luajit
 brew install homebrew/dupes/grep
 brew install homebrew/dupes/openssh
 brew install homebrew/dupes/screen
