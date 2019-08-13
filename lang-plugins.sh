cd60798385b6b146f4df4ec337aa1c7ddd51f83a
Mario Yepes <marioy47@gmail.com>
2019-08-13 08:50:38 -0500

    Removing deprecated vscode plugins

--- lang-plugins.sh
+++ lang-plugins.sh
@@ -1,7 +1,6 @@
 # NPM Commands
 npm install -g gulp
 npm install -g scalablepath-toggl
-npm install -g wunderline
 
 # Vagrant plugins
 vagrant plugin install vagrant-bindfs
@@ -16,7 +15,6 @@ code --install-extension CoenraadS.bracket-pair-colorizer
 code --install-extension jdinhlife.gruvbox
 code --install-extension PKief.material-icon-theme
 code --install-extension sdras.night-owl
-code --install-extension shobhit01.cobalt2-italic
 code --install-extension smlombardi.slime
 code --install-extension Serge.vsc-material-theme-italicize
 
@@ -29,7 +27,6 @@ code --install-extension Tyriar.sort-lines
 # VSCode PHP
 code --install-extension rifi2k.format-html-in-php
 code --install-extension neilbrayfield.php-docblocker
-code --install-extension MehediDracula.php-namespace-resolver
 code --install-extension fterrag.vscode-php-cs-fixer
 code --install-extension bmewburn.vscode-intelephense-client
 
