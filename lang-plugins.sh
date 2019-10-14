aaf47c57e89ee39a49d46e59df82915bdb5a5073
Mario Yepes <marioy47@gmail.com>
2019-10-14 16:58:00 -0500

    Removing unused files

--- lang-plugins.sh
+++ lang-plugins.sh
@@ -1,6 +1,6 @@
 # NPM Commands
 npm install -g gulp
-npm install -g scalablepath-toggl
+#npm install -g scalablepath-toggl
 
 # Vagrant plugins
 vagrant plugin install vagrant-bindfs
@@ -11,22 +11,32 @@ vagrant plugin install vagrant-bindfs
 # code --install-extension yzhang.markdown-all-in-one
 
 # VSCode Theming
-code --install-extension CoenraadS.bracket-pair-colorizer
 code --install-extension jdinhlife.gruvbox
 code --install-extension PKief.material-icon-theme
 code --install-extension sdras.night-owl
 code --install-extension smlombardi.slime
-code --install-extension Serge.vsc-material-theme-italicize
 
 # VSCode Productivity
 code --install-extension patbenatar.advanced-new-file
+code --install-extension formulahendry.auto-rename-tag
+code --install-extension CoenraadS.bracket-pair-colorizer
 code --install-extension sleistner.vscode-fileutils
-code --install-extension vscodevim.vim
+code --install-extension ionutvmi.path-autocomplete
+code --install-extension esbenp.prettier-vscode
+code --install-extension alefragnani.project-manager
+code --install-extension humao.rest-client
 code --install-extension Tyriar.sort-lines
+code --install-extension ban.spellright
+code --install-extension vscodevim.vim
+code --install-extension wordpresstoolbox.wordpress-toolbox
+code --install-extension Shan.code-settings-sync
 
 # VSCode PHP
-code --install-extension rifi2k.format-html-in-php
 code --install-extension neilbrayfield.php-docblocker
-code --install-extension fterrag.vscode-php-cs-fixer
 code --install-extension bmewburn.vscode-intelephense-client
+code --install-extension wongjn.php-sniffer
+
+# VSCode Misc
+code --install-extension Shan.code-settings-sync # Backup Settings
+
 
