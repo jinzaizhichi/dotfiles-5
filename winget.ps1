310df81a9f9f858b705f164d08cddbaa4c72c78f
Mario Yepes <marioy47@gmail.com>
2021-06-01 09:56:36 -0500

    Adding winget list of files

---
+++ winget.ps1
@@ -0,0 +1,30 @@
+wignet install Bitwarden.Bitwarden
+winget install 7zip.7zip
+winget install BinaryFortress.DisplayFusion
+winget install BraveSoftware.BraveBrowser
+winget install Canonical.Ubuntu
+winget install Docker.DockerDesktop
+winget install Foxit.FoxitReader
+winget install Greenshot.Greenshot
+winget install Logitech.Options
+winget install Microsoft.VisualStudio.2019.Community
+winget install Microsoft.WindowsTerminal
+winget install Mozilla.Firefox
+winget install Postman
+winget install SlackTechnologies.Slack
+winget install Spotify.Spotify
+winget install SublimeHQ.SublimeText.4
+winget install Twilio.Authy
+winget install VMware.HorizonClient
+winget install VideoLAN.VLC
+winget install Zoom.Zoom
+winget install calibre.calibre
+winget install qBittorrent.qBittorrent
+winget install seer
+
+echo "Additional Items:"
+echo Trello
+echo Seer
+echo Harvest
+echo Commics++
+echo Todoist
