2b50d1a78d23869c15e3e7b3d74405e672b6dc32
Mario Yepes <marioy47@gmail.com>
2022-07-15 15:28:22 -0500

    Cleaning up vs plugin list and adding -brew mas- commans to the update

--- plugins.sh
+++ plugins.sh
@@ -9,33 +9,4 @@ composer global require squizlabs/php_codesniffer
 
 # Visual Studio Code
 # Rebuild with: `code --list-extensions | xargs -L 1 echo code --install-extension`
-# code --install-extension alefragnani.project-manager
-# code --install-extension asvetliakov.vscode-neovim
-# code --install-extension atomiks.moonlight
-# code --install-extension ban.spellright
-# code --install-extension bierner.markdown-preview-github-styles
-# code --install-extension bmewburn.vscode-intelephense-client
-# code --install-extension CoenraadS.bracket-pair-colorizer-2
-# code --install-extension Dart-Code.dart-code
-# code --install-extension Dart-Code.flutter
-# code --install-extension dbaeumer.vscode-eslint
-# code --install-extension DotJoshJohnson.xml
-# code --install-extension EditorConfig.EditorConfig
-# code --install-extension Equinusocio.vsc-material-theme
-# code --install-extension esbenp.prettier-vscode
-# code --install-extension humao.rest-client
-# code --install-extension ionutvmi.path-autocomplete
-# code --install-extension joaomoreno.github-sharp-theme
-# code --install-extension jpotterm.simple-vim
-# code --install-extension mtxr.sqltools
-# code --install-extension neilbrayfield.php-docblocker
-# code --install-extension patbenatar.advanced-new-file
-# code --install-extension PKief.material-icon-theme
-# code --install-extension sdras.night-owl
-# code --install-extension sensourceinc.vscode-sql-beautify
 code --install-extension Shan.code-settings-sync
-# code --install-extension sirmspencer.vscode-autohide
-# code --install-extension sleistner.vscode-fileutils
-# code --install-extension teabyii.ayu
-# code --install-extension Tyriar.sort-lines
-# code --install-extension wongjn.php-sniffer
